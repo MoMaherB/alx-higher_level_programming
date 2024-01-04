41770fde565392140b4244993b065a6768f0c64b
MoMaherB <maher1525@gmail.com>
2024-01-04 09:35:46 -0800

    Python - import & modules Python

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    argv = sys.argv
+    if len(argv) == 1:
+        print("0 arguments.")
+    else:
+        if len(argv) == 2:
+             print("{:d} argument:".format(len(argv) - 1))
+        else:
+            print("{:d} arguments:".format(len(argv) - 1))
+        for i in range(1, len(argv)):
+             print("{:d}: {:s}".format(i, argv[i])) 
