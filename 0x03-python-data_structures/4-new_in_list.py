31e4b5b0ba0bc7feeb082ba88f596a6b8d868a02
MoMaherB <maher1525@gmail.com>
2024-01-06 06:01:07 -0800

    Python - Data Structures: Lists, Tuples

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
+    if not my_list:
+        return None
     if idx > 0 and len(my_list) > idx:
         new_list = my_list.copy()
         new_list[idx] = element
         return new_list
-
     return my_list
