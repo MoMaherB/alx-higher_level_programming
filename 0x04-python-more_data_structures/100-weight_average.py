09a49b1a7eb0425e163a1d296da366e87ddf80c1
MoMaherB <maher1525@gmail.com>
2024-01-10 15:06:13 -0800

    Python - More Data Structures: Set, Dictionary

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    if my_list is None:
+    if not my_list:
         return 0
     score = my_list[0][0] * my_list[0][1]
     weight = my_list[0][1]
