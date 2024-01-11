7ad284d415a0d0944b5c7de82acda6b748987776
MoMaherB <maher1525@gmail.com>
2024-01-10 16:15:28 -0800

    Python - More Data Structures: Set, Dictionary

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    if type(roman_string) is not str:
+        return 0
     rdic = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     sum = rdic[roman_string[0]]
     for i in range(1, len(roman_string)):
