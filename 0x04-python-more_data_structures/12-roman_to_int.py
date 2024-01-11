8dc1797414364c6e2068ed8f5644d0134a743ad9
MoMaherB <maher1525@gmail.com>
2024-01-10 16:11:36 -0800

    Python - More Data Structures: Set, Dictionary

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    rdic = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    sum = rdic[roman_string[0]]
+    for i in range(1, len(roman_string)):
+        if rdic[roman_string[i]] <= rdic[roman_string[i - 1]]:
+            sum = sum + roman_dict[roman_string[i]]
+        else:
+            sum = sum + rdic[roman_string[i]] - 2 * rdic[roman_string[i - 1]]
+    return sum
