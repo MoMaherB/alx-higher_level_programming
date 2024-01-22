685c0ae518ab01ba3cbf60d0d288fc8413d3f546
MoMaherB <maher1525@gmail.com>
2024-01-22 09:40:11 -0800

    Python - Exceptions

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,8 +3,8 @@ def safe_print_list(my_list=[], x=0):
     try:
         while (i < x):
             print(my_list[i], end="")
-            i +=1
-    except:
+            i += 1
+    except IndexError:
         pass
     print("")
     return i
