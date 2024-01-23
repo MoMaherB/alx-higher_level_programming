807b0954369239a8cafaa49ada614267ab93ef8f
MoMaherB <maher1525@gmail.com>
2024-01-23 09:09:45 -0800

    Python - Classes and Objects

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Class Square"""
+
+
+class Square:
+    """Class Square
+    Args:
+        size: the size of the sauare
+    Raises:
+        TypeError: if the size is not int.
+        ValueError: if the size is less than zero.
+    """
+    def __init__(self, size=0):
+        if size != int(size):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
