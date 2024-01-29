46dd792ca4e83e2ee7b55e20cc3e037ffcfe6b72
MoMaherB <maher1525@gmail.com>
2024-01-29 10:32:14 -0800

    Python - More Classes and Objects

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -110,7 +110,7 @@ class Rectangle:
 
         for i in range(self.__height):
             for j in range(self.__width):
-                rectangle.append(str(Rectangle.print_symbol))
+                rectangle.append(str(self.print_symbol))
             rectangle.append("\n")
 
         rectangle.pop()
