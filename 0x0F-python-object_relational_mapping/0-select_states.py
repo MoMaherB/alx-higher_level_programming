f90e1bcd8af5036066d7ec0237457ab6ec738d1d
MoMaherB <maher1525@gmail.com>
2024-06-23 04:11:03 +0300

    Python - Object-relational mapping

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -2,17 +2,18 @@
 """  lists all states from the database hbtn_0e_0_usa """
 import MySQLdb
 import sys
-db = MySQLdb.connect(
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
     host="localhost",
     user=sys.argv[1],
     passwd=sys.argv[2],
     db=sys.argv[3]
 )
-if __name__ == "__main__":
     cursor = db.cursor()
     cursor.execute("SELECT * FROM states")
-    rows = cursor.fetchall()
-    for row in rows:
+    #rows = cursor.fetchall()
+    for row in cursor:
         print(row)
     cursor.close()
     db.close()
