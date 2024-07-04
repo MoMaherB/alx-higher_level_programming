2e51183da8f723d0e16184415e86ec217024530d
MoMaherB <maher1525@gmail.com>
2024-07-04 21:57:54 +0300

    Python - Network #0

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -5,20 +5,19 @@
 def find_peak(list_of_integers):
     """Finds a peak in list_of_integers"""
 
-    if list_of_integers is None or list_of_integers == []:
+    if not list_of_integers:
         return None
-    lo = 0
-    hi = len(list_of_integers)
-    mid = ((hi - lo) // 2) + lo
-    mid = int(mid)
-    if hi == 1:
-        return list_of_integers[0]
-    if hi == 2:
-        return max(list_of_integers)
-    if list_of_integers[mid] >= list_of_integers[mid - 1] and\
-            list_of_integers[mid] >= list_of_integers[mid + 1]:
-        return list_of_integers[mid]
-    if mid > 0 and list_of_integers[mid] < list_of_integers[mid + 1]:
-        return find_peak(list_of_integers[mid:])
-    if mid > 0 and list_of_integers[mid] < list_of_integers[mid - 1]:
-        return find_peak(list_of_integers[:mid])
+
+    def find_peak_recursive(nums, low, high):
+        if low == high:
+            return nums[low]
+
+        mid = (low + high) // 2
+
+        if mid < len(nums) - 1 and nums[mid] < nums[mid + 1]:
+            return find_peak_recursive(nums, mid + 1, high)
+        if mid > 0 and nums[mid] < nums[mid - 1]:
+            return find_peak_recursive(nums, low, mid - 1)
+        return nums[mid]
+
+    return find_peak_recursive(list_of_integers, 0, len(list_of_integers) - 1)
