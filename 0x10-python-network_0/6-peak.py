b07ac283e6ffd23304064d7fc3f4e00f31f32b93
MoMaherB <maher1525@gmail.com>
2024-07-04 21:50:55 +0300

    Python - Network #0

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -7,11 +7,13 @@ def find_peak(list_of_integers):
         return None
 
     def my_peak(nums, low, high):
-        if low == high:
-            return nums[low]
         mid = (low + high) // 2
-        if nums[mid] < nums[mid + 1]:
+        if (mid == 0 or nums[mid] >= nums[mid - 1]) and\
+                (mid == len(nums) - 1 or nums[mid] >= nums[mid + 1]):
+            return nums[mid]
+        elif mid > 0 and nums[mid - 1] > nums[mid]:
+            return my_peak(nums, low, mid - 1)
+        else:
             return my_peak(nums, mid + 1, high)
-        return my_peak(nums, low, mid)
 
     return my_peak(list_of_integers, 0, len(list_of_integers) - 1)
