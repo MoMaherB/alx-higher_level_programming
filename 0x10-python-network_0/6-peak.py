ae370eccc0dfa9e1268d87b7d108aa193284218c
MoMaherB <maher1525@gmail.com>
2024-07-04 21:53:46 +0300

    Python - Network #0

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -6,14 +6,19 @@ def find_peak(list_of_integers):
     if not list_of_integers:
         return None
 
-    def my_peak(nums, low, high):
-        mid = (low + high) // 2
-        if (mid == 0 or nums[mid] >= nums[mid - 1]) and\
-                (mid == len(nums) - 1 or nums[mid] >= nums[mid + 1]):
-            return nums[mid]
-        elif mid > 0 and nums[mid - 1] > nums[mid]:
-            return my_peak(nums, low, mid - 1)
-        else:
-            return my_peak(nums, mid + 1, high)
+    low = 0
+    high = len(list_of_integers) - 1
 
-    return my_peak(list_of_integers, 0, len(list_of_integers) - 1)
+    def find_peak_recursive(nums, low, high):
+        if low == high:
+            return nums[low]
+
+        mid = (low + high) // 2
+
+        if nums[mid] < nums[mid + 1]:
+            return find_peak_recursive(nums, mid + 1, high)
+        return find_peak_recursive(nums, low, mid)
+
+    return find_peak_recursive(list_of_integers, low, high)
+
+
