0bbf870dcef8cf5c2dca169b72d51015142f762a
MoMaherB <maher1525@gmail.com>
2024-07-05 18:24:41 +0300

    Python - Network #1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,9 +7,11 @@ fetches https://alx-intranet.hbtn.io/status.uses urlib package
 if __name__ == '__main__':
     import urllib.request
 
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as result:
+    with urllib.request.urlopen(
+            'https://alx-intranet.hbtn.io/status')as result:
         responce = result.read()
         print("Body response:")
         print("\t- type: {}".format(type(responce)))
         print("\t- content: {}".format(responce))
-        print("\t- utf8 content: {}".format(responce.decode('utf-8')))
+        print("\t- utf8 content: {}".format(
+            responce.decode('utf-8')))
