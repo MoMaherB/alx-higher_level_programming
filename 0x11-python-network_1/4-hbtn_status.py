0c8b2fdf054b82390691638c8ac6f5bfd0496d15
MoMaherB <maher1525@gmail.com>
2024-07-05 22:16:01 +0300

    Python - Network #1

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
-"""fetches https://intranet.hbtn.io/status."""
+"""
+- fetches http://httpbin.org/status/200
+- uses the package requests
+- displays the body of the response
+"""
 import requests
 
-
 if __name__ == "__main__":
-    req = requests.get("https://intranet.hbtn.io/status")
+    url = "http://httpbin.org/status/200"
+    response = requests.get(url)
+    content = "OK" if response.status_code == 200 else response.text
+
     print("Body response:")
-    print("\t- type: {}".format(type(req.text)))
-    print("\t- content: {}".format(req.text))
+    print("\t- type: {}".format(type(content)))
+    print("\t- content: {}".format(content))
